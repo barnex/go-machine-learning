ebab2a6d1f399edf66d3a70fd3d8554a20475b11
Arne Vansteenkiste <barnex@gmail.com>
2018-06-03 21:31:00 +0200

    refactor numerical gradient

--- grad.go
+++ grad.go
@@ -1,18 +1,24 @@
 package vs
 
-//func GradW(dst []float64, f Net, w, x []float64, label int) float64 {
-//	const delta = 1. / (1024 * 1024)
-//	for i := range dst {
-//		backup := w[i]
-//
-//		w[i] = backup - delta
-//		v1 := f.Eval(w, x)
-//
-//		w[i] = backup + delta
-//		v2 := f.Eval(w, x)
-//
-//		dst[i] = (v2 - v1) / (2 * delta)
-//		w[i] = backup
-//	}
-//	panic("TODO")
-//}
+// GradLogits numerically approximates component i of f's gradient with respect to w.
+func GradLogits(y []float64, f Net, w, x []float64, i int) {
+	netCheckSize(f, y, w, x)
+	const delta = 1. / (1024 * 1024)
+
+	y1 := y
+	y2 := make([]float64, len(y))
+
+	backup := w[i]
+
+	w[i] = backup - delta
+	f.Logits(y1, w, x)
+
+	w[i] = backup + delta
+	f.Logits(y2, w, x)
+
+	w[i] = backup
+
+	for j := range y {
+		y[j] = (y2[j] - y1[j]) / (2 * delta)
+	}
+}
