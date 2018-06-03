048e119b40ef2fb099feaa7edebdbae64e8e15e3
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2018-06-03 15:35:27 +0200

    refactor

---
+++ img/img_test.go
@@ -0,0 +1,13 @@
+package img_test
+
+import "github.com/barnex/vectorstream/img"
+
+func ExampleImgFromSlice() {
+	list := []float64{1, 2, 3, 4, 5, 6}
+	img := img.FromSlice(list, 2, 3)
+	img.Print()
+
+	//Output:
+	//1.00 2.00 3.00
+	//4.00 5.00 6.00
+}
---
+++ test.go
@@ -0,0 +1,18 @@
+package vs
+
+//import "math"
+
+//func Infer(m Net, w []float64, x []float64) int {
+//	assert(len(testdata) > 0)
+//
+//	var nHit int
+//	guess := make([]float64, m.NumLabels())
+//	for _, limg := range testdata {
+//		assert(len(limg.Img.List) > 0)
+//		m.Infer(guess, limg.Img)
+//		if ArgMax(guess) == limg.Label {
+//			nHit++
+//		}
+//	}
+//	return nHit
+//}
